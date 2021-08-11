b7400f0818995db707a1f7c164fbb29c76144a9f
Daniel Santos <daniel.santos@octopass.com.br>
2021-08-11 15:52:30 -0300

    feat: add weight to heading font

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 $(() => {
 
-    const DEFAULT_OPACITY = '20%'
-
-    /**
-     * Wordpress connection
-     */
-
-    const wpApiSettings = {
-        root: 'https://teste.octopass.com.br/wp-json',
-    }
-
+    // App container
     const app = document.querySelector("#app");
 
+    // Default opacity
+    const DEFAULT_OPACITY = '20%'
 
+    // Changes opacity
     const changeBerlindaOpacity = opacity => {
         // Set berlinda opacity
         app.querySelector("#berlinda").style.opacity = opacity;
@@ -21,27 +15,31 @@ $(() => {
         app.querySelector("#percentage").innerText = opacity;
     }
 
+    /**
+     * Wordpress connection
+     */
+
+    // Wordpress config
+    const wpApiSettings = {
+        root: 'https://teste.octopass.com.br/wp-json',
+        productID: '265488'
+    }
+
     // AJAX request
 
     $.ajax({
-        url: wpApiSettings.root + '/wp/v2/product/265488',
+        url: wpApiSettings.root + '/wp/v2/product/' + wpApiSettings.productID,
         method: 'GET',
     }).done(function (response) {
         const fundingInfo = response['wpcf_product']
         let raisedPercentToNumber = fundingInfo['raised_percent']
 
-        console.log(raisedPercentToNumber)
 
         if (raisedPercentToNumber > '20%') {
             changeBerlindaOpacity(fundingInfo['raised_percent'])
         } else {
             changeBerlindaOpacity(DEFAULT_OPACITY)
         }
-        
-
-
-
-
     });
 
 
