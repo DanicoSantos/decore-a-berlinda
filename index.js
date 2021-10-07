1a5ab18b732d26d684b23b3ec7c9ca539a5e7013
Daniel Santos <daniel.santos@octopass.com.br>
2021-10-07 11:10:21 -0300

    fix: change flower to amount proportion

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ $(() => {
     // Changes greyscale percentage
     const changeBerlindaStatus = info => {
 
-        const raisedFlowers = parseFloat(info['total_raised']) / 7.5;
+        const raisedFlowers = parseFloat(info['total_raised']) / 10;
 
         // Change percentage inner text
         app.querySelector("#percentage").innerText = parseInt(raisedFlowers) + " Flores";
@@ -59,7 +59,7 @@ $(() => {
                 changeBerlindaStatus(fundingInfo)
             }
 
-            const flowersRaised = parseFloat(fundingInfo['total_raised']) / 7.5;
+            const flowersRaised = parseFloat(fundingInfo['total_raised']) / 10;
 
             if (0 < flowersRaised <= 10000) {
                 index = 3;
